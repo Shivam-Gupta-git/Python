9de312da2aa5d0751374d2e51abd427e94d35f89
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-31 16:02:21 +0530

    Please provide the git diff so I can help you create an appropriate commit message.

---
+++ 01_Basic/hello_chai.py
@@ -0,0 +1,10 @@
+print("chai or code")
+ 
+def chai(n):
+    print(n)
+
+chai('lemon tea')    
+
+chai_one = "lemon tea"
+chai_two = "green tea"
+chai_three = "masala tea"
---
+++ notes/notes.py
@@ -0,0 +1,45 @@
+# shivamgupta@Shivams-MacBook-Air Python % ls
+# 01_Basic
+# shivamgupta@Shivams-MacBook-Air Python % cd 01_Basic
+# shivamgupta@Shivams-MacBook-Air 01_Basic % python3
+# Python 3.14.2 (v3.14.2:df793163d58, Dec  5 2025, 12:18:06) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+
+# >>> import os
+# >>> os.sys()
+
+# Traceback (most recent call last):
+#   File "<python-input-1>", line 1, in <module>
+#     os.sys()
+#     ~~~~~~^^
+# TypeError: 'module' object is not callable
+# >>> os.sys()
+# Traceback (most recent call last):
+#   File "<python-input-2>", line 1, in <module>
+#     os.sys()
+#     ~~~~~~^^
+# TypeError: 'module' object is not callable
+
+# >>> import sys
+# >>> sys.platform
+# 'darwin'
+
+# >>> import hello_chai
+# chai or code
+# lemon tea
+# >>> hello_chai.chai("mint chai")
+# mint chai
+
+
+# >>> from importlib import reload
+# >>> reload(hello_chai)
+# chai or code
+# lemon tea
+# <module 'hello_chai' from '/Users/shivamgupta/Desktop/Python/01_Basic/hello_chai.py'>
+# >>> hello_chai.chai_one
+# 'lemon tea'
+# >>> hello_chai.chai_two
+# 'green tea'
+# >>> hello_chai.chai_three
+# 'masala tea'
+
