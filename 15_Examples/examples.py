15fdd548a9599088af7049d91a21006ad6e78721
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-03-03 15:42:56 +0530

    examples

---
+++ 15_Examples/examples.py
@@ -0,0 +1,56 @@
+# 1. Numeric Data Types.......................
+# i. Int............
+# numbers = 12
+# print(numbers)
+
+# ii. Float..........
+# numbers = 12.7
+# print(numbers)
+
+# iii. complex.......
+# numbers = 3 + 4j
+# print(numbers)
+
+# 2. Sequence Data Types...........................
+# i. String...........
+# name = "python"
+# print(name)
+
+# Slicing......
+# text = "shivam"
+# print(text[0:3])
+
+# Methods of String.................
+# language = "python"
+# print(language.lower())
+
+# print(language.upper())
+
+# print(language.capitalize())
+
+# print(language.title())
+
+# language = "p y t h o n"
+# print(language.strip())
+
+
+# String Containing...................
+
+# name = "Python is a programming language"
+
+# # print("Python" in name) // True
+# # print("Java" in name) // False
+
+# ii. List ............
+# numbers = [1, 2, 3, 4]
+# print(numbers) 
+
+# print(numbers[0])  // 1
+# print(numbers[-1])  // 4
+# print(numbers[2]) // 3
+
+# List Slicing......
+# numbers = [1, 5, 9, 2, 7]
+
+# print(numbers[0 : 2])  // [1, 5]
+# print(numbers[1:4]) // [5, 9, 2]
