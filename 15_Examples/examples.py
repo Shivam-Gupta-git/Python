be8c87c97fd5446854d3e2d4a6cbb9384805c939
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-03-03 16:58:05 +0530

    Add examples of tuples, ranges, dictionaries, sets, and loops in examples.py

--- 15_Examples/examples.py
+++ 15_Examples/examples.py
@@ -54,3 +54,52 @@
 
 # print(numbers[0 : 2])  // [1, 5]
 # print(numbers[1:4]) // [5, 9, 2]
+
+# iii. Ttuple............
+# students = ('saurav', 'raman', 'ravi', 'mohit', 'pawan')
+# print(students)
+
+# tange ..........
+# r = range(1, 5)
+# print(list(r)) // [1, 2, 3, 4]
+
+# 3. Mapping Data Type :.......................
+
+# student = {
+#   "name" : "shivam",
+#   "age" : 22,
+#   "cource" : "python"
+# }
+# print(student)
+# print(student["name"])
+
+# 4. Set Data Types...................
+# numbers = [1, 3, 4, 2, 6, 3]
+# print(frozenset(numbers))
+
+# Difference between == and is.....................
+# x = 20
+# y = 30
+
+# print( x is y)
+
+# Example of for loop..................................
+# with list......
+# numbers = [1, 3, 2, 5, 4, 7]
+# for num in numbers:
+#   print(num) # 1 3 2 5 4 7
+
+# with range.......
+# for num in range(0, 5):
+#   print(num)  # 0, 1, 2, 3, 4 
+
+# with string
+# name = "pythin"
+# for words in name:
+#   print(words) # p y t h o n
+
+# Example of while loop...................
+num = 1
+while num <= 5 :
+  num += 1
+  print(num)  # 2 3 4 5 6
